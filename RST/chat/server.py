e3c07fcd424994ea75fe74d23f6bf88cc4f0116b
Googlya <tails4321@yandex.ru>
2019-12-23 19:35:51 +0500

    Update server.py

--- RST/chat/server.py
+++ RST/chat/server.py
@@ -1,14 +1,14 @@
 import socket # Создаём сокет для подключения
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind (('0.0.0.0',11141)) # Прослушиваем все интерфейсы по порту 11141 
+sock.bind (('0.0.0.0',11141)) # Прослушиваем все интерфейсы по порту 11141
 client = [] # Массив где храним адреса клиентов
 print ('Start Server')
 while 1 :
          data , addres = sock.recvfrom(1024)
          print (addres[0], addres[1])
-         if  addres not in client : 
-                 client.append(addres)# Если такова клиента нету , то добавить
+         if  addres not in client :
+                 client.append(addres)# Если такого клиента нет, то добавить
          for clients in client :
-                 if clients == adress : 
+                 if clients == addres :
                      continue # Не отправлять данные клиенту который их прислал
                  sock.sendto(data,clients)

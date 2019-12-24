e6a6c9f42952b52f6d0bbdc15cc5ae3841c72350
Googlya <tails4321@yandex.ru>
2019-12-24 14:31:01 +0500

    Update Firebird.py

--- RST/Firebird/Firebird.py
+++ RST/Firebird/Firebird.py
@@ -3,20 +3,18 @@ import getpass
 
 print('Введите путь до базы(Используйте localhost при подключении к локальной базе)')
 path = input()
-kov_path = repr(str(path))
 print('Введите логин')
 username = input()
-kov_user = repr(str(username))
 print('Введите пароль')
 password = getpass.getpass()
-kov_pass = repr(str(password))
-print(password)
-con = fdb.connect(dsn= kov_path, user= kov_user, password=kov_pass)
+con = fdb.connect(dsn=path, user=username, password=password)
 
-cur = con.cursor() #Cursos объект
+cur = con.cursor()
 
-tables = cur.execute("show tables;")
-print(tables) #Вывод таблиц из базы
-SELECT = cur.execute("select * from test;")
-#Вывод информации из таблицы тест
-print(SELECT)
+cur.execute("SELECT * FROM RDB$RELATIONS")
+print(cur.fetchall())
+
+print('______________________')
+
+cur.execute("SELECT * FROM test")
+print(cur.fetchmany(5))
